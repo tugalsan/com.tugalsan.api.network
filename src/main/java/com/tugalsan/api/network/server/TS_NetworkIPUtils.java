185592ecd2831d438599844426253e2a695b6f79
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-24 12:29:16 +0300

    Update TS_NetworkIPUtils.java

--- src/main/java/com/tugalsan/api/network/server/TS_NetworkIPUtils.java
+++ src/main/java/com/tugalsan/api/network/server/TS_NetworkIPUtils.java
@@ -1,7 +1,7 @@
 package com.tugalsan.api.network.server;
 
 import com.tugalsan.api.callable.client.TGS_CallableType1;
-import com.tugalsan.api.charset.client.TGS_CharSetCast;
+import com.tugalsan.api.charset.client.TGS_CharSet;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.network.client.TGS_NetworkIPUtils;
 import com.tugalsan.api.os.server.TS_OsProcess;
@@ -134,7 +134,7 @@ public class TS_NetworkIPUtils {
     }
 
     public static String get_IP_CONFIG_ALL() {//cmd /c netstat
-        var osName = TGS_CharSetCast.toLocaleLowerCase(System.getProperty("os.name"));
+        var osName = TGS_CharSet.cmn().languageDefault().toLowerCase(System.getProperty("os.name"));
         if (osName.startsWith("windows")) {
             return TS_OsProcess.of("ipconfig /all").output;
         }
