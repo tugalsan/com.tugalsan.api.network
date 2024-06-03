6fa6c984d0e1d6ae877cb810d3162d653156f470
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-03 11:59:40 +0300

    Update TS_NetworkIPs.java

--- src/main/java/com/tugalsan/api/network/server/TS_NetworkIPs.java
+++ src/main/java/com/tugalsan/api/network/server/TS_NetworkIPs.java
@@ -1,6 +1,7 @@
 package com.tugalsan.api.network.server;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public record TS_NetworkIPs(Optional<String> ip_loopback,
@@ -8,4 +9,30 @@ public record TS_NetworkIPs(Optional<String> ip_loopback,
         List<String> ip_castMulti,
         List<String> ip_hostLocal,
         List<String> ip_hostPublic) {
+
+    public boolean ip_hostPublic_contains(String ipTarget) {
+        return ip_hostPublic.stream().filter(ipSource -> Objects.equals(ipSource, ipTarget)).findAny().isPresent();
+    }
+
+    public boolean ip_hostLocal_contains(String ipTarget) {
+        return ip_hostLocal.stream().filter(ipSource -> Objects.equals(ipSource, ipTarget)).findAny().isPresent();
+    }
+
+    public boolean ip_castMulti_contains(String ipTarget) {
+        return ip_castMulti.stream().filter(ipSource -> Objects.equals(ipSource, ipTarget)).findAny().isPresent();
+    }
+
+    public boolean ip_castBroad_contains(String ipTarget) {
+        if (ip_castBroad.isEmpty()) {
+            return false;
+        }
+        return Objects.equals(ip_castBroad.orElseThrow(), ipTarget);
+    }
+
+    public boolean ip_loopback_contains(String ipTarget) {
+        if (ip_loopback.isEmpty()) {
+            return false;
+        }
+        return Objects.equals(ip_loopback.orElseThrow(), ipTarget);
+    }
 }
