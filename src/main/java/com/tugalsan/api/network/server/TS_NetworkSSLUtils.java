ce20bd6d28f925ca71a5401b164a89db3ff51ae4
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-05 13:36:31 +0300

    .

--- src/main/java/com/tugalsan/api/network/server/TS_NetworkSSLUtils.java
+++ src/main/java/com/tugalsan/api/network/server/TS_NetworkSSLUtils.java
@@ -1,6 +1,7 @@
 package com.tugalsan.api.network.server;
 
 import com.tugalsan.api.function.client.TGS_Func;
+import com.tugalsan.api.stream.client.TGS_StreamUtils;
 import com.tugalsan.api.time.client.TGS_Time;
 import com.tugalsan.api.union.client.TGS_UnionExcuse;
 import com.tugalsan.api.unsafe.client.*;
@@ -11,9 +12,50 @@ import java.nio.file.Path;
 import javax.net.ssl.*;
 import java.security.*;
 import java.security.cert.*;
+import java.security.cert.Certificate;
 import java.util.*;
 
 public class TS_NetworkSSLUtils {
+
+    public static TGS_UnionExcuse<KeyStore> sniffFromP12ToKeyStore(Path p12, CharSequence pass) {
+        return TGS_UnSafe.call(() -> {
+            var store = KeyStore.getInstance("PKCS12");
+            store.load(Files.newInputStream(p12), pass.toString().toCharArray());
+            return TGS_UnionExcuse.of(store);
+        }, e -> TGS_UnionExcuse.ofExcuse(e));
+    }
+
+    public static TGS_UnionExcuse<PrivateKey> sniffFromP12ToPrivateKey(Path p12, CharSequence pass) {
+        return TGS_UnSafe.call(() -> {
+            var u_store = sniffFromP12ToKeyStore(p12, pass);
+            if (u_store.isExcuse()) {
+                TGS_UnSafe.thrw(u_store.excuse());
+            }
+            u_store.value().load(Files.newInputStream(p12), pass.toString().toCharArray());
+            var privateKey = (PrivateKey) u_store.value().getKey("example", pass.toString().toCharArray());
+            if (privateKey == null) {
+                TGS_UnSafe.thrw(new NullPointerException("no private key exists"));
+            }
+            return TGS_UnionExcuse.of(privateKey);
+        }, e -> TGS_UnionExcuse.ofExcuse(e));
+    }
+
+    public static TGS_UnionExcuse<List<Certificate>> sniffFromToCertificatesP12(Path p12, CharSequence pass, boolean withChain) {
+        return TGS_UnSafe.call(() -> {
+            List<Certificate> certificates = new ArrayList();
+            var u_store = sniffFromP12ToKeyStore(p12, pass);
+            if (u_store.isExcuse()) {
+                TGS_UnSafe.thrw(u_store.excuse());
+            }
+            var aliases = TGS_StreamUtils.of(u_store.value().aliases()).toList();
+            for (var alias : aliases) {
+                var cert_main = u_store.value().getCertificate(alias);
+                certificates.add(cert_main);
+                certificates.addAll(Arrays.asList(u_store.value().getCertificateChain(alias)));
+            }
+            return TGS_UnionExcuse.of(certificates);
+        }, e -> TGS_UnionExcuse.ofExcuse(e));
+    }
 
     public static TGS_UnionExcuse<TGS_Time> expirationOfPem(Path pemPath) {
         try {
